340c75bc76d5d9eee9c10986f2798b7dbb4b8c74
EmperorOlashinbo <ib.olasunbo@gmail.com>
2025-09-02 12:00:16 +0000

    Updates Theme.kt.

--- app/src/main/java/com/example/applepierecipeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/applepierecipeapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.applepierecipeapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,34 +8,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF8B4513),
+    secondary = Color(0xFF6B4E31),
+    tertiary = Color(0xFFF5E8C7)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Color(0xFF8B4513),
+    secondary = Color(0xFF6B4E31),
+    tertiary = Color(0xFFF5E8C7)
 )
 
 @Composable
 fun ApplePieRecipeAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -45,7 +34,6 @@ fun ApplePieRecipeAppTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
